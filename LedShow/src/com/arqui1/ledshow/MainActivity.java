8bed005f294b74fd467ba219600f62747393fdfc
Alex508vv <Alex508vv@gmail.com>
2014-05-06 20:09:48 -0600

    Activity esstilo de fuentesss

--- LedShow/src/com/arqui1/ledshow/MainActivity.java
+++ LedShow/src/com/arqui1/ledshow/MainActivity.java
@@ -1,11 +1,15 @@
 package com.arqui1.ledshow;
 
+//import com.wifigt.scaner.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +21,27 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_fuente:
+			this.lanzarActividadFuentes();
+			return true;
+		case R.id.menu_efecto:
+			return true;
+		case R.id.menu_direccion:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
+	private void lanzarActividadFuentes(){
+		Intent intent = new Intent(getApplicationContext(), FuentesActivity.class);
+		startActivity(intent);
 	}
 
 }
